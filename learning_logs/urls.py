fe90bd5ebbb497dee6df7090e74e9f92b0195395
Gerard Di Mattia <gerard.dimattia@jjay.cuny.edu>
2023-06-27 02:11:49 -0400

    adding topic form.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # Detail page for a single topic.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topics/', views.new_topics, name='new_topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """The home page for Learning Log."""
@@ -18,3 +19,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('id')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topics(request):
+    """Add a new Topic."""
+    if request.method != 'POST':
+        # No data submitted; created a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.saved()
+            return redirect('learning_logs:topics')
+    
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
